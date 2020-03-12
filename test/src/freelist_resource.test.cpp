94c958b396539e6e76aac0a77a66df768cd4e683
Kevin Alexandre Boissonneault <kaboissonneault@bhvr.com>
2020-03-12 16:46:18 -0400

    Added more freelist_resource tests

--- test/src/freelist_resource.test.cpp
+++ test/src/freelist_resource.test.cpp
@@ -1,24 +1,23 @@
 #include <memory/resource_reference.h>
 #include <memory/freelist_resource.h>
-#include <memory/malloc_resource.h>
 
 #include <catch.hpp>
 
 #include "test_resource.h"
 
-TEST_CASE("Freelist resource", "[memory]")
-{
-	constexpr size_t BlockSize = 64;
-	using freelist_resource = kab::freelist_resource<kab::resource_reference<test_resource>, BlockSize>;
+constexpr size_t BlockSize = 64;
+using freelist_resource = kab::freelist_resource<kab::resource_reference<test_resource>, BlockSize>;
 
+TEST_CASE("Freelist BlockSize Alloc", "[memory]")
+{	
 	test_resource tester;
-	freelist_resource freelist(tester);
-
-	// Expect no allocations so far
-	REQUIRE(tester.get_total_alloc() == 0);
-
-	// Test "BlockSize" sized allocations
+	
 	{
+		freelist_resource freelist(tester);
+
+		// Expect no allocations so far
+		REQUIRE(tester.get_total_alloc() == 0);
+
 		const kab::byte_span first_alloc = freelist.allocate(BlockSize);
 
 		REQUIRE(first_alloc.size == BlockSize); // 'allocate' always returns the requested size
@@ -40,9 +39,77 @@ TEST_CASE("Freelist resource", "[memory]")
 		REQUIRE(tester.get_current_alloc() == 0);
 
 		freelist.deallocate(freelist.allocate(BlockSize));
-		freelist.~freelist_resource();
+	}
+
+	REQUIRE(tester.get_current_alloc() == 0);
+
+}
+
+TEST_CASE("Freelist Multi Alloc", "[memory]")
+{
+	test_resource tester;
+
+	{
+		freelist_resource freelist(tester);
+
+		constexpr size_t AllocCount = 10;
+		kab::byte_span allocations[AllocCount];
+		for (kab::byte_span& alloc : allocations)
+		{
+			alloc = freelist.allocate(BlockSize);
+		}
+
+		REQUIRE(tester.get_current_alloc() == AllocCount * BlockSize);
+		for (kab::byte_span alloc : allocations)
+		{
+			REQUIRE(alloc.size == BlockSize);
+		}
+
+		for (kab::byte_span alloc : allocations)
+		{
+			freelist.deallocate(alloc);
+		}
+
+		REQUIRE(tester.get_current_alloc() == AllocCount * BlockSize);
+
+		const kab::byte_span freelist_head = freelist.allocate(BlockSize);
+		REQUIRE(freelist_head.data == allocations[AllocCount - 1].data); // head of the freelist should be the last deallocation
+		freelist.deallocate(freelist_head);
+	}
+
+	REQUIRE(tester.get_current_alloc() == 0);
+}
+
+TEST_CASE("Freelist Small Alloc", "[memory]")
+{
+	test_resource tester;
+
+	{
+		freelist_resource freelist(tester);
+
+		constexpr size_t SmallAlloc = BlockSize / 2;
+		const kab::byte_span first_alloc = freelist.allocate(SmallAlloc);
+
+		REQUIRE(first_alloc.size == SmallAlloc); // 'allocate' always returns the requested size
+		REQUIRE(tester.get_last_alloc() == BlockSize); // freelist only allocates in chunks of "BlockSize"
+		REQUIRE(tester.get_current_alloc() == BlockSize);
+
+		freelist.deallocate(first_alloc);
+
+		REQUIRE(tester.get_current_alloc() == BlockSize); // freelist never frees chunks that fit the block size
+
+		const kab::byte_span second_alloc = freelist.allocate(SmallAlloc);
+
+		REQUIRE(tester.get_current_alloc() == BlockSize); // expect no new allocations: we have to recover the last deallocated block
+		REQUIRE(second_alloc.data == first_alloc.data); // expect to get back the same block
+
+		freelist.deallocate(second_alloc);
+
+		freelist.clear();
 		REQUIRE(tester.get_current_alloc() == 0);
 
-		new(&freelist) freelist_resource(tester);
+		freelist.deallocate(freelist.allocate(BlockSize));
 	}
+
+	REQUIRE(tester.get_current_alloc() == 0);
 }
--- test/src/test_resource.h
+++ test/src/test_resource.h
@@ -11,6 +11,10 @@ class test_resource
 	size_t m_current_alloc = 0;
 	size_t m_total_alloc = 0;
 public:
+	test_resource() = default;
+	test_resource(test_resource const&) = delete;
+	test_resource& operator=(test_resource const&) = delete;
+
 	[[nodiscard]] kab::byte_span allocate(size_t n)
 	{
 		m_last_alloc = n;
@@ -32,4 +36,9 @@ public:
 	size_t get_current_alloc() const noexcept { return m_current_alloc; }
 	// Get the total number of bytes ever allocated
 	size_t get_total_alloc() const noexcept { return m_total_alloc; }
+
+	[[nodiscard]] bool operator==(test_resource const& rhs) const noexcept
+	{
+		return &rhs == this;
+	}
 };
