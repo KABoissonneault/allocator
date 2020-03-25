f33693a4dae18fda7d0b4c28aa6205fe9c0cdc57
Kevin Alexandre Boissonneault <kaboissonneault@bhvr.com>
2020-03-25 15:37:41 -0400

    Added memory_resource concept documentation

--- include/memory/resource.h
+++ include/memory/resource.h
@@ -44,4 +44,45 @@ namespace kab
 	{
 		return n != 0 && ((n & (n - 1)) == 0);
 	}
+
+	/** concept memory_resource
+	 * 
+	 *  A memory_resource type is a type which has access to a region of heap and can allocate (and deallocate) sections of it on demand
+	 *
+	 *  The essential functions are:
+	 *      byte_span allocate(size_t s, align_t a)
+	 *          - Which is an allocation function
+	 *          - Where 's' is the requested size of the allocation
+	 *          - Where 'a' is the requested alignment of the allocation
+	 *          - The returned byte_span has a size (ie: returned size) equal to the requested size
+	 *          - The returned byte_span has a pointer to storage (ie: returned storage) with alignment that respects the requested alignment
+	 *      void deallocate(byte_span s, align_t a)
+	 *          - Which is a deallocation function
+	 *          - Where 's' is a span with the values returned by a call to an allocation function
+	 *          - Where 'a' was the alignment requested on the allocation function
+	 *  
+	 *  Memory resources can have extensions.
+	 *  
+	 *  Relocatable
+	 *
+	 *  If the memory resource is relocatable, it must be trivially relocatable.
+	 *
+	 *  Moveable
+	 * 
+	 *  A memory resource is moveable if it is noexcept move constructible, and noexcept move assignable
+	 *
+	 *  Copyable
+	 *
+	 *  A memory resource is copyable if it is moveable, noexcept copy constructible, and noexcept copy assignable
+	 *
+	 *  Over Allocator
+	 *  
+	 *  An over-allocator memory resource is a memory resource that supports the following functions:
+	 *      byte_span over_allocate(size_t s, align_t a)
+	 *          - Which is an allocation function. It behaves like 'allocate', except the returned size can be bigger than the requested size
+	 *          - On deallocation, the span provided to the deallocation function must still be the returned one, not the requested one
+	 *      void over_deallocate(byte_span s, align_t a)
+	 *          - Which is an optional deallocation function. If provided, the user must call this instead of 'deallocate' when using over-allocations
+	 *          - Otherwise, behaves like 'deallocate'
+	 */
 }
