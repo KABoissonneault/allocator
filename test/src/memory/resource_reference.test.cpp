e5bbac2151a24ae15a09aa456faa37f06b5c82b6
Kevin Alexandre Boissonneault <kaboissonneault@bhvr.com>
2020-03-30 15:05:49 -0400

    Added resource_reference compilation tests

---
+++ test/src/memory/resource_reference.test.cpp
@@ -0,0 +1,20 @@
+#include "memory/resource_reference.h"
+
+#include <catch.hpp>
+
+struct simple_alloc
+{
+	kab::byte_span allocate(kab::size_t, kab::align_t);
+	void deallocate(kab::byte_span, kab::align_t) noexcept;
+};
+
+TEST_CASE("Resource Reference Compilation", "[memory]")
+{
+	using resource1 = kab::resource_reference<simple_alloc>;
+
+	REQUIRE(!std::is_default_constructible_v<resource1>);
+	REQUIRE(std::is_nothrow_copy_constructible_v<resource1>);
+	REQUIRE(std::is_nothrow_copy_assignable_v<resource1>);
+	REQUIRE(std::is_nothrow_move_constructible_v<resource1>);
+	REQUIRE(std::is_nothrow_move_assignable_v<resource1>);
+}
