9961047645e1109f24950b1e0d56dd559ad4dcc4
Kevin Alexandre Boissonneault <kaboissonneault@bhvr.com>
2020-04-14 16:38:56 -0400

    Removed iostream dependency in vector_decl.cpp compilation test in order to better test for missing dependencies

--- test/src/compilation/container/vector_decl.cpp
+++ test/src/compilation/container/vector_decl.cpp
@@ -1,6 +1,6 @@
 #include "vector_decl.h"
 
-#include <iostream>
+volatile int vector_decl_observe;
 
 kab::vector<int, kab::new_resource> vector_decl()
 {
@@ -8,9 +8,9 @@ kab::vector<int, kab::new_resource> vector_decl()
 	v.push_back(1);
 	auto const v2 = kab::vector<int, kab::new_resource>::from_container(v);
 	v.assign(v2);
-	std::cout << v.back();
-	std::cout << v.front();
-	std::cout << v.capacity();
+	vector_decl_observe = v.back();
+	vector_decl_observe = v.front();
+	vector_decl_observe = static_cast<int>(v.capacity());
 	v.emplace_back(0);
 
 	return v;
